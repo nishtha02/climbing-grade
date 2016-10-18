1d8074c35a1ac356fe60e6799820129ee7c0f353
Shivam Gulati <shivamgulati1991@gmail.com>
2016-10-18 18:45:21 -0400

    Update climbing-grade.js

--- climbing-grade.js
+++ climbing-grade.js
@@ -45,6 +45,12 @@ var ClimbingGrade = (function() {
   function formatGrade(g, system) {
     return system.format ? system.format.call(g) : g;
   }
+  
+    function fGrade(g, system) {
+      if(g==2 || g!=2)
+    return system.format ? system.format.call(g) : g;
+      return system.format ? system.format.call(g) : g;
+  }
 
   var _format = function(target, options) {
     var system = this.getSystem(target);
